29aad01e1c5e87c7128cd2211c196db5392414b4
Priyanka <77085180+pkamble35@users.noreply.github.com>
2021-06-30 01:41:16 -0400

    added default home routing

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ function App() {
     <Router>
     <Navbar />
       <Switch >
+      <Route path = "/" component = {Home}/>
         <Route path = "/home" component = {Home}/>
         <Route path = "/location" component = {Location} />
         <Route path = "/sports" component = {Sports} />
