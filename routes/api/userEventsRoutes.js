5539559615a211d0fc3f69f469957275646c0ba6
Ken Otuo <mrkoajr@gmail.com>
2021-06-30 20:39:17 -0400

    adding dotenv file and modifying the routes

--- routes/api/userEventsRoutes.js
+++ routes/api/userEventsRoutes.js
@@ -1,14 +1,12 @@
 const router = require('express').Router();
-const UserEvents = require('../../models/UserEvents');
-router.get("/userEvent", async (req, res) => {
-    try {
-      
-      res.json("userEvent");
-    }
-    catch (err) {
-      res.status(400).json(err);
-    }
-  });
+const { UserEvents } = require('../../models');
 
-
-module.exports = router;
+// User-saved fav evnets
+router.post('/', async (req, res) => {
+  try {
+    const userEventsData = await UserEvents.create(req.body);
+    res.status(200).json(userEventsData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
